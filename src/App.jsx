98f368c9aeb352b8d90a5cc25d3f2b6d470d84ae
Dhanush Adhithyan <dhanush.adhithyan@realiti.io>
2025-01-09 12:59:07 +0530

    Sibin || comments sections

--- src/App.jsx
+++ src/App.jsx
@@ -56,12 +56,12 @@ const App = () => {
             <Education />
           </div>
         </div>
-        <Achievements />
+        {/* <Achievements /> */}
         <div className={`bg-primary ${styles.flexCenter} ${styles.paddingX}`}>
           <div className={`${styles.boxWidth}`}>
             <Projects />
             <BlogPosts enabled={false} />
-            <OpenSource />
+            {/* <OpenSource /> */}
             <ExtraCurricular />
           </div>
         </div>
